d1df7c8c003d09868923dc25fc18d6a25b4e194f
VedanshMaheshwari <vedanshm15@gmail.com>
2026-01-17 22:00:35 +0530

    structuring

--- src/app.js
+++ src/app.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const connectDB = require("./config/database");
 const app = express();
+const User = require("./models/User");
+
+app.post("/signup", async (rew,res)=>{
+    const obj = new User({
+        firstName: "John",
+        lastName: "Doe",
+        emailID: "john@gmail.com",
+        password: "password123",
+        age: 30,
+        gender: "Male",
+    });
+
+    try{
+        await obj.save();
+        res.send("User signed up successfully");
+    }catch(err){
+        res.status(400).send("Error signing up user: " + err.message);
+    }
+}) 
+
 
 connectDB().then(()=>{
     console.log('Database connected successfully');
